0fb490a3f2eb98b42872f4f632f9463b07211202
Matthieu Claessens <matthieu-claessens@hotmail.com>
2025-09-12 11:10:54 +0200

    Added servlets, dao, models

---
+++ src/main/java/com/webbiblio/model/Book.java
@@ -0,0 +1,26 @@
+package com.webbiblio.model;
+
+import jakarta.persistence.*;
+
+// Entité JPA représentant un livre
+@Entity
+@Table(name = "books")
+public class Book{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto-increment
+    private Long id;
+
+    private String name;
+
+    public Book() {}
+
+    public Book(String name) {
+        this.name = name;
+    }
+
+    // Getters et Setters
+    public Long getId() { return id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -2,15 +2,15 @@
 <%@ taglib uri="jakarta.tags.core" prefix="c" %>
 <!-- Page JSP simple affichant la liste des utilisateurs et un formulaire d'ajout -->
 <html>
-<head><title>Users</title></head>
+<head><title>Auteurs</title></head>
 <body>
-<h2>Add User</h2>
+<h2>Ajouter un auteur</h2>
 <form method="post" action="users">
     <input type="text" name="name" placeholder="Name"/>
     <button type="submit">Save</button>
 </form>
 
-<h2>User List</h2>
+<h2>Liste d'auteur</h2>
 <ul>
     <c:forEach var="user" items="${users}">
         <li>${user.name}</li>
